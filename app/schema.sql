ce3175591df38596b610247607e90ec6cb68c190
Pavel Škop <pskop.k10@gmail.com>
2022-12-02 20:50:30 +0100

    odstranění zbytečností

--- app/schema.sql
+++ app/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS record (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
-  descr TEXT NOT NULL,
+  popis TEXT NOT NULL,
   jazyk TEXT NOT NULL,
   hodnoceni INT NOT NULL
 );
-insert into record (name, descr, jazyk, hodnoceni) values ('Dan', 'Ano', 'C++', 5);
-insert into record (name, descr, jazyk, hodnoceni) values ('Dan', 'Ano', 'C++', 5);
-insert into record (name, descr, jazyk, hodnoceni) values ('Dan', 'Ano', 'C++', 5);
+insert into record (name, popis, jazyk, hodnoceni) values ('Dan', 'Ano', 'C++', 5);
+insert into record (name, popis, jazyk, hodnoceni) values ('Idiot', 'Ano', 'php', 1);
+insert into record (name, popis, jazyk, hodnoceni) values ('Dan', 'Ne', 'C++', 5);
