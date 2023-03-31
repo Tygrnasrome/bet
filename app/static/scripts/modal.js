4484d796e067182be3b1be5235cb491b832f88f4
Robert Ernest <robert.ernest@mensa.cz>
2023-03-31 18:41:10 +0200

    AAAAAAAAA

--- app/static/scripts/modal.js
+++ app/static/scripts/modal.js
@@ -30,7 +30,7 @@ var modal = document.getElementById("myModalU");
 var btn = document.getElementById("MyUpdate");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
