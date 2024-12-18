05943962c607ae669347d237fc1cc891f11ec9a6
polaracep <adam@beyer.cz>
2024-12-18 16:22:56 +0100

    Something working server

--- server.py
+++ server.py
@@ -1,28 +1,43 @@
-# Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
 hostName = "0.0.0.0"
 serverPort = 8080
+base_directory = "templates"
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        file_path = self.path.strip("/")
+        if file_path == "":
+            file_path = "index.html"
+        
+        full_path = os.path.join(base_directory, file_path)
+        try:
+            if os.path.isfile(full_path):
+                with open(full_path, "rb") as file:
+                    content = file.read()
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(content)
+            else:
+                self.send_response(404)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(b"<html><body><h1>404 Not Found</h1></body></html>")
+        except Exception as e:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>500 Internal Server Error</h1></body></html>")
 
-if __name__ == "__main__":        
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
-
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
-
     webServer.server_close()
     print("Server stopped.")
+
